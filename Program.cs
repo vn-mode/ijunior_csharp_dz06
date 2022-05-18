adfd2a8b6e06f9bbbb81ae84be57d2c8b6e22513
VITALII NAGIBIN <nagibin87@yandex.ru>
2022-05-18 21:29:09 +0300

    dz06
    dz06

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace vn_mode_csharp_dz06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gold = 1;
+            int countGold;
+            int ourCrystals;
+            int priceOneCrystal = 3 * gold;
+
+            Console.Write("Сколько у вас в кошельке золота? : ");
+            countGold = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Сегодня у нас акция и 1 кристалл  = {priceOneCrystal} золота.");
+            Console.Write("Сколько кристаллов вы хотите купить? : ");
+            ourCrystals = Convert.ToInt32(Console.ReadLine());
+
+            countGold -= ourCrystals * priceOneCrystal;
+
+            Console.WriteLine($"У вас в кошельке теперь {ourCrystals} кристаллов и {countGold} золота.");
+        }
+    }
+}
